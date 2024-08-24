19c56236beda7ec55393129098c8e30573381577
KacperSobolak <kacpersobolak03@gmail.com>
2024-08-24 16:21:30 +0200

    Add project files.

---
+++ InvoiceBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    internal class InvoiceBuilder
+    {
+        private Invoice _invoice = new Invoice();
+
+        public Invoice Build()
+        {
+            return _invoice;
+        }
+
+        public void SetVendor(string vendor)
+        {
+            _invoice.Vendor = vendor;
+        }
+
+        public void SetVendee(string vendee)
+        {
+            _invoice.Vendor = vendee;
+        }
+
+        public void SetNote(string note)
+        {
+            _invoice.Note = note;
+        }
+
+        public void SetDate(DateTime date)
+        {
+            _invoice.Date = date;
+        }
+
+        public void SetLineItems(IEnumerable<string> lineItems)
+        {
+            _invoice.LineItems = lineItems;
+        }
+
+        public void SetNumber(string number)
+        {
+            _invoice.Number = number;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Builder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var invoiceBuilder = new InvoiceBuilder();
+
+            invoiceBuilder.SetNote("Test note");
+            invoiceBuilder.SetNumber("434");
+
+            var invoice = invoiceBuilder.Build();
+        }
+    }
+}   
