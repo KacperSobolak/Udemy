ea13e89b34d95908a638323b714c35a249e6348d
KacperSobolak <kacpersobolak03@gmail.com>
2024-08-25 22:53:53 +0200

    singleton

---
+++ DesignPatterns/Creational/Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var cfg = new Configuration();
+            var cfg = Configuration.GetInstance();
+            var cfg2 = Configuration.GetInstance();
+
+            Console.WriteLine(ReferenceEquals(cfg, cfg2));
+        }
+    }
+}
