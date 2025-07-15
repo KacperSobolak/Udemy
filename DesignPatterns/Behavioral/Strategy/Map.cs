8b88e7eb0dd03d054b0ddc3de8a30fbe8d5c4152
Kacper Sobolak <sobolak@student.agh.edu.pl>
2025-07-15 19:01:07 +0200

    Strategy

---
+++ DesignPatterns/Behavioral/Strategy/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    internal class Map(IRouteStrategy routeStrategy)
+    {
+        private readonly IRouteStrategy _routeStrategy = routeStrategy;
+
+        public void CreateRoute(Cordinate start, Cordinate end)
+        {
+            _routeStrategy.CreateRoute(start, end);
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Strategy/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var strategy = new CarStrategy();
+
+            var map = new Map(strategy);
+
+            var start = new Cordinate();
+            var end = new Cordinate();
+
+            map.CreateRoute(start, end);
+        }
+    }
+}
